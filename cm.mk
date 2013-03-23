5004866b48063a96f91ac81447201d865fbbd44c
Matt Filetto <matt.filetto@gmail.com>
2013-03-23 05:02:19 -0700

    glacier: Cleanup cm.mk
    Change-Id: I311904c414ca6e0eb4f4f969831385997d3cc502

--- cm.mk
+++ cm.mk
@@ -5,6 +5,8 @@ $(call inherit-product, device/htc/glacier/device.mk)
 $(call inherit-product, vendor/cm/config/gsm.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+PRODUCT_RELEASE_NAME := glacier
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 480
@@ -14,7 +16,7 @@ TARGET_SCREEN_WIDTH := 480
 #
 PRODUCT_NAME := cm_glacier
 PRODUCT_DEVICE := glacier
-PRODUCT_BRAND := T-Mobile
+PRODUCT_BRAND := HTC
 PRODUCT_MODEL := myTouch 4G
 PRODUCT_MANUFACTURER := HTC
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_glacier BUILD_FINGERPRINT=htc_europe/htc_primou/primou:4.0.3/IML74K/39988.1:user/release-keys PRIVATE_BUILD_DESC="2.19.531.1 CL95282 release-keys"
