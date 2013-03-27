a3027598292afbca19de932935cc0adebda3e195
Matt Filetto <matt.filetto@gmail.com>
2013-03-27 09:34:29 -0700

    glacier: remove a couple unused build prop settings
    Change-Id: I7fe2f1989a1d1f0abd434eb61a1d0ba45dce3e16

--- device.mk
+++ device.mk
@@ -27,8 +27,6 @@ PRODUCT_COPY_FILES += \
 # Build.prop additions
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.locationfeatures=1 \
-    ro.com.google.networklocation=1 \
-    ro.com.google.gmsversion=4.0_r1 \
     ro.setupwizard.enable_bypass=1 \
     dalvik.vm.lockprof.threshold=500 \
     dalvik.vm.dexopt-flags=m=y
