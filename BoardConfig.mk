6631467792a0c9e95b33341eb0802f42584a88b2
Matt Filetto <matt.filetto@gmail.com>
2013-08-11 00:39:05 -0700

    Remove a few things to get us booting for now. (WIP)
    Change-Id: I8f11b332ebf30ca08ebcdb7d995b1d3e94385628

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,5 +82,5 @@ BOARD_HAS_NO_MISC_PARTITION := true
 TARGET_RECOVERY_FSTAB = device/htc/glacier/ramdisk/fstab.glacier
 RECOVERY_FSTAB_VERSION := 2
 
-BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun0/file
+#BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+#TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun0/file
