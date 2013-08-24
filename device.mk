e15a0f95a7f919646ad259ce877122e3437cae41
Matt Filetto <matt.filetto@gmail.com>
2013-08-23 21:52:27 -0700

    remove liblights and libsensors and use ones from msm7x30-common instead
    Change-Id: I2a3b8533d02346c132578213b5cd2ae66063e819

--- device.mk
+++ device.mk
@@ -43,10 +43,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/htc/glacier/configs/voicemail-conf.xml:system/etc/voicemail-conf.xml
 
-# Hal
+# GPS
 PRODUCT_PACKAGES += \
-    lights.glacier \
-    sensors.glacier \
     gps.glacier
 
 # Torch
